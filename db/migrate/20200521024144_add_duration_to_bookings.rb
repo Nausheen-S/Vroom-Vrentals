cdc5ea4f9ef8683f82bc47a6c184680aaea9c239
Nausheen_S <salauddinnausheen@gmail.com>
2020-05-21 16:43:44 +0800

    altered migrations

--- db/migrate/20200521024144_add_duration_to_bookings.rb
+++
@@ -1,5 +0,0 @@
-class AddDurationToBookings < ActiveRecord::Migration[5.2]
-  def change
-    add_column :bookings, :duration, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_21_030748) do
+ActiveRecord::Schema.define(version: 2020_05_21_044755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
